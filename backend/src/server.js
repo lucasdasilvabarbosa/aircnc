e86cff45c1f96ef25b912fca8baf0b29d64161c2
lucasdasilvabarbosa <dasilvabarbosalucas@gmail.com>
2019-10-03 00:05:16 -0400

    iniciando projeto ominiStack

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-8epyc.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
