47c170952ff79c8ff0d1a4da2e85d15ee9fa2d4f
unknown <dasilvabarbosalucas@gmail.com>
2019-11-13 00:37:54 -0400

    iniciando frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css'
+import logo from './assets/logo.svg'
+
+function App() {
+    return(
+        <div className="container">
+          <img src={logo} alt="Aircnc"></img>
+
+          <div className="content">
+              <p>
+                  Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
+              </p>
+          
+
+              <form>
+                  <label htmlFor="email">E-MAIL *</label>
+                  <input type="email" id="email" placeholder="seu melhor e-mail"/>
+          
+                  <button className="btn" type="submit">Entrar</button>
+              </form>
+          </div>
+        </div>
+    );
+}
+
+export default App;
