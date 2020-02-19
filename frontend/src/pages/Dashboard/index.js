b7246f151a956e6dc6b1416777dc7f4bddce135d
Lucas <barbosa.94>
2020-02-19 10:45:47 -0400

    add paginas que serão criadas, e finalizando login

---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import api from '../../services/api';
+
+export default function Dashboard() {
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/spots', {
+                headers: { user_id }
+            });
+
+            console.log(response)
+        }
+
+        loadSpots();
+    }, []);
+
+
+    return <div>Dashboard</div>
+}
