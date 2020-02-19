1cdb2dfb9b603d4f71e346268225493e6ca3efc6
Lucas <barbosa.94>
2020-02-19 10:44:35 -0400

    add cors no backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-8epyc.mongodb.net/semana0
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,17 @@ import React from 'react';
 import './App.css'
 import logo from './assets/logo.svg'
 
+import Routes from './routes';
+
 function App() {
+
     return(
         <div className="container">
           <img src={logo} alt="Aircnc"></img>
 
           <div className="content">
-              <p>
-                  Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
-              </p>
-          
-
-              <form>
-                  <label htmlFor="email">E-MAIL *</label>
-                  <input type="email" id="email" placeholder="seu melhor e-mail"/>
-          
-                  <button className="btn" type="submit">Entrar</button>
-              </form>
+              <Routes/>
+              
           </div>
         </div>
     );
